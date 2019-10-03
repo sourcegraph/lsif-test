86a0432f06a8aa1b8f869bd2f161a8ab76b9244b
Eric Fritz <eric@eric-fritz.com>
2019-10-03 10:15:46 -0500

    Add arg parser.

--- cmd/lsif-validate/main.go
+++ cmd/lsif-validate/main.go
@@ -5,26 +5,48 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/alecthomas/kingpin"
 	"github.com/sourcegraph/lsif-test/assets"
 	"github.com/sourcegraph/lsif-test/validation"
 	"github.com/xeipuuv/gojsonschema"
 )
 
+const version = "0.1.0"
+
 func main() {
-	if err := run(); err != nil {
-		fmt.Printf("error: %s\n", err.Error())
+	if err := realMain(); err != nil {
+		fmt.Fprint(os.Stderr, fmt.Sprintf("error: %v\n", err))
 		os.Exit(1)
 	}
 }
 
-func run() error {
-	validator, err := getValidator()
+func realMain() error {
+	app := kingpin.New("lsif-go", "lsif-validate is validator for LSIF indexer output.").Version(version)
+	dumpFile := app.Arg("dump-file", "The LSIf output to validate.").Default("data.lsif").File()
+
+	_, err := app.Parse(os.Args[1:])
 	if err != nil {
-		return fmt.Errorf("get validator: %v", err)
+		return err
 	}
 
-	if err := validate(os.Args[1], validator.ValidateLine); err != nil {
-		return err
+	defer (*dumpFile).Close()
+
+	schema, err := getSchema()
+	if err != nil {
+		return fmt.Errorf("schema: %v", err)
+	}
+
+	validator := validation.NewValidator(schema)
+
+	scanner := bufio.NewScanner(*dumpFile)
+	for scanner.Scan() {
+		if err := validator.ValidateLine(scanner.Text()); err != nil {
+			return err
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("scanner: %v", err)
 	}
 
 	if err := validator.ValidateGraph(); err != nil {
@@ -34,47 +56,11 @@ func run() error {
 	return nil
 }
 
-func getValidator() (*validation.Validator, error) {
-	schema, err := getSchema()
-	if err != nil {
-		return nil, fmt.Errorf("get schema: %v", err)
-	}
-
-	return validation.NewValidator(schema), nil
-}
-
 func getSchema() (*gojsonschema.Schema, error) {
 	content, err := assets.Asset("lsif.schema.json")
 	if err != nil {
-		return nil, fmt.Errorf("asset: %v", err)
+		return nil, err
 	}
 
-	schema, err := gojsonschema.NewSchema(gojsonschema.NewStringLoader(string(content)))
-	if err != nil {
-		return nil, fmt.Errorf("new schema: %v", err)
-	}
-
-	return schema, nil
-}
-
-func validate(filename string, validator validation.LineValidator) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("os open: %v", err)
-	}
-
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		if err := validator(scanner.Text()); err != nil {
-			return err
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		return fmt.Errorf("scanner: %v", err)
-	}
-
-	return nil
+	return gojsonschema.NewSchema(gojsonschema.NewStringLoader(string(content)))
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/sourcegraph/lsif-test
 go 1.13
 
 require (
+	github.com/alecthomas/kingpin v2.2.6+incompatible
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/kevinburke/go-bindata v3.14.0+incompatible // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.1.0
